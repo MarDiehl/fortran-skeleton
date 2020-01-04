55b6daeb2b5f4240b9a5a575d946a39acb26032e
Martin Diehl <mail@martin-diehl.net>
2020-01-04 15:35:41 +0100

    version without PETSc

---
+++ without-PETSc/run.sh
@@ -0,0 +1,12 @@
+if [ -z ${F90+x} ]; 
+    then export F90=gfortran
+fi
+if [ -z ${CC+x} ]; 
+   then export CC=gcc
+fi
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=DEBUG -DOPTIMIZATION=OFF -DOPENMP=OFF -DCMAKE_Fortran_COMPILER=$F90 -DCMAKE_C_COMPILER=$CC
+make
+./src/executable
